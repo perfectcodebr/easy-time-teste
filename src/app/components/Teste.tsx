a3f11d4c7bfbf2da5d905b5ee7b6c771951b7cfa
jonathanfront <jonathanrj_2010@live.com>
2024-06-28 19:10:07 -0300

    adicionando animaçoes no css

--- src/app/components/Teste.tsx
+++ src/app/components/Teste.tsx
@@ -1,5 +1,26 @@
 export default function Teste() {
     return (
-        <h1>Teste</h1>
+        <div className="divprincipal">
+            <img src="/image/fotocard.png" alt="fotocard" />
+            <p className="titulo1">Corte Low Fade</p>
+            <div className="preço">
+                <div>
+                    <p>R$ 35,00</p>
+                </div>
+
+                <div className="divconteudo">
+                    <p>-15%</p>
+                </div>
+
+            </div>
+            <hr />
+            <div >
+            <div className="conteudo1">
+                <img src="/image/bross.png" alt="foto.empresa" />
+                
+                    <h2>Bross Back´s Pro...</h2>
+                </div>
+            </div>
+        </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import Teste from "./components/Teste";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="grid grid-cols-3 min-h-screen  gap-10 items-center justify-between p-24">
+      <Teste/>
+      <Teste/>
+      <Teste/>
+      <Teste/>
+      <Teste/>
+      <Teste/>
+      <Teste/>
+      <Teste/>
+
+
+
     </main>
   );
 }
